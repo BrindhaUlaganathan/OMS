9bd50f12c2f400a8f3e8be6aaca4ad921ccfdecf
BrindhaUlaganathan <92741736+BrindhaUlaganathan@users.noreply.github.com>
2023-04-04 18:31:50 +0100

    Add files via upload

---
+++ OmsPlacingOrderController.java
@@ -0,0 +1,23 @@
+package com.brindha.oms.controller;
+
+import com.brindha.oms.model.OrderAcknowledgement;
+import com.brindha.oms.model.PlacingOrder;
+import com.brindha.oms.service.PlacingOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class OmsPlacingOrderController {
+    @Autowired
+    private PlacingOrderService poservice;
+
+
+    @RequestMapping(value="/placeorder", method= {RequestMethod.POST})
+    public OrderAcknowledgement placeorder(@RequestBody PlacingOrder placingorderrequest) {
+        return  poservice.placeOrder(placingorderrequest);
+    }
+}
