d30288590193677f271c9db6aed47ac246de160e
BrindhaUlaganathan <92741736+BrindhaUlaganathan@users.noreply.github.com>
2023-04-04 18:33:28 +0100

    Add files via upload

--- src/main/java/com/brindha/oms/controller/OmsCustomerController.java
+++ src/main/java/com/brindha/oms/controller/OmsCustomerController.java
@@ -3,24 +3,18 @@ package com.brindha.oms.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.brindha.oms.service.OrderAcknowledgement;
-import com.brindha.oms.service.PlacingOrder;
 import com.brindha.oms.model.Customer;
-import com.brindha.oms.model.OrderLine;
-import com.brindha.oms.model.Products;
-import com.brindha.oms.model.PurchaseOrder;
 import com.brindha.oms.service.OmsCustomerService;
-import com.brindha.oms.service.OmsOrderLineService;
-import com.brindha.oms.service.OmsProductService;
-import com.brindha.oms.service.OmsPurchaseOrderService;
-import com.brindha.oms.service.PlacingOrderService;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+//1
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api")
@@ -28,23 +22,29 @@ public class OmsCustomerController {
 	
 	@Autowired
 	OmsCustomerService omsCustomerService;
-	
-	@Autowired
-	private PlacingOrderService poservice;
-	
-
-	@RequestMapping(value="/placeorder", method= {RequestMethod.POST})
-	public OrderAcknowledgement placeorder(@RequestBody PlacingOrder placingorderrequest) {
-		return  poservice.placeorder(placingorderrequest);		
-	}
     
     @RequestMapping(value="/customer", method= {RequestMethod.POST})
-    public Customer createEmployee(@RequestBody Customer cust) {
-		return omsCustomerService.createCustomer(cust);
-    }
+
+	public ResponseEntity<Object> createCustomer(@RequestBody Customer cust) {
+
+		omsCustomerService.createCustomer(cust);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{customerNo}")
+				.buildAndExpand(cust.getCustomerNo())
+				.toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+
+//    public Customer createCustomer(@RequestBody Customer cust) {
+//
+//		return omsCustomerService.createCustomer(cust);
+//    }
    
     @RequestMapping(value="/customer", method=RequestMethod.GET)
-    public List<Customer> readEmployees() {
+    public List<Customer> readCustomers() {
+
 		return omsCustomerService.getCustomers();
     }
 
@@ -55,59 +55,8 @@ public class OmsCustomerController {
 
 	@RequestMapping(value="/customer/{customerNo}", method=RequestMethod.DELETE)
 	public void deleteCustomer(@PathVariable(value = "customerNo") int id) {
+
 		omsCustomerService.deleteCustomer(id);
 	}
-	
-	@Autowired
-	OmsOrderLineService omsOrderLineService;
-     
-    @RequestMapping(value="/orderline", method= {RequestMethod.POST})
-    
-    public OrderLine createOrderLineService(@RequestBody OrderLine ol) {
-        return omsOrderLineService.createOrderLine(ol);
-    }
-   
-    @RequestMapping(value="/orderline", method=RequestMethod.GET)
-    public List<OrderLine> readOrderLineService() {
-		return omsOrderLineService.getOrderLine();
-    }
-
-
-    @Autowired
-	OmsProductService omsProductService;
-    
-    @RequestMapping(value="/products", method= {RequestMethod.POST})    
-    public Products createProduct(@RequestBody Products prod) {
-		return omsProductService.createProduct(prod);
-    }
-   
-    @RequestMapping(value="/products", method=RequestMethod.GET)
-    public List<Products> getProducts() {
-		return omsProductService.getProducts();
-    }
-
-	@RequestMapping(value="/products/{uPC}", method=RequestMethod.PUT)
-	public Products readProducts(@PathVariable(value = "uPC") int id, @RequestBody Products prodDetails) {
-	   return omsProductService.updateProducts(id, prodDetails);
-	 }
-
-	@RequestMapping(value="/products/{uPC}", method=RequestMethod.DELETE)
-	public void deleteProducts(@PathVariable(value = "uPC") int id) {
-		omsProductService.deleteProducts(id);
-	}
-	
-	
-	@Autowired
-	OmsPurchaseOrderService omsPurchaseOrderService;
-     
-    @RequestMapping(value="/purchaseorder", method= {RequestMethod.POST})
-    public PurchaseOrder createPurchaseOrder(@RequestBody PurchaseOrder po) {
-        return omsPurchaseOrderService.createPurchaseOrder(po);
-    }
-   
-    @RequestMapping(value="/purchaseorder", method=RequestMethod.GET)
-    public List<PurchaseOrder> readPurchaseOrder() {
-		return omsPurchaseOrderService.getPurchaseOrder();
-    }
 
 }
